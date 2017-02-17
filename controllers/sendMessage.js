e383673d693f076ad102717279cabade3df056bc
fshaik <faisalshaik@gmail.com>
2017-02-16 23:17:33 -0500

    completed sendmesage controller

--- controllers/sendMessage.js
+++ controllers/sendMessage.js
@@ -135,19 +135,193 @@ module.exports = {
   },
 
   /*
-   * Send a video using the Send API.
+  * Send a video using the Send API.
+  *
+  */
+  sendVideoMessage: function (recipientId) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      attachment: {
+        type: "video",
+        payload: {
+          url: SERVER_URL + "/assets/allofus480.mov"
+        }
+      }
+    }
+  };
+
+  callSendAPI(messageData);
+  },
+
+  /*
+  * Send a video using the Send API.
+  *
+  */
+  sendFileMessage: function (recipientId, url) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      attachment: {
+        type: "file",
+        payload: {
+          url: url
+        }
+      }
+    }
+  };
+
+  callSendAPI(messageData);
+  },
+    /*
+   * Send a button message using the Send API.
    *
    */
-  sendVideoMessage: function (recipientId) {
+  function sendButtonMessage(recipientId) {
     var messageData = {
       recipient: {
         id: recipientId
       },
       message: {
         attachment: {
-          type: "video",
+          type: "template",
           payload: {
-            url: SERVER_URL + "/assets/allofus480.mov"
+            template_type: "button",
+            text: "This is test text",
+            buttons:[{
+              type: "web_url",
+              url: "https://www.oculus.com/en-us/rift/",
+              title: "Open Web URL"
+            }, {
+              type: "postback",
+              title: "Trigger Postback",
+              payload: "DEVELOPED_DEFINED_PAYLOAD"
+            }, {
+              type: "phone_number",
+              title: "Call Phone Number",
+              payload: "+16505551234"
+            }]
+          }
+        }
+      }
+    };  
+
+    callSendAPI(messageData);
+  },
+
+  /*
+   * Send a Structured Message (Generic Message type) using the Send API.
+   *
+   */
+  sendGenericMessage: function (recipientId) {
+    var messageData = {
+      recipient: {
+        id: recipientId
+      },
+      message: {
+        attachment: {
+          type: "template",
+          payload: {
+            template_type: "generic",
+            elements: [{
+              title: "rift",
+              subtitle: "Next-generation virtual reality",
+              item_url: "https://www.oculus.com/en-us/rift/",               
+              image_url: SERVER_URL + "/assets/rift.png",
+              buttons: [{
+                type: "web_url",
+                url: "https://www.oculus.com/en-us/rift/",
+                title: "Open Web URL"
+              }, {
+                type: "postback",
+                title: "Call Postback",
+                payload: "Payload for first bubble",
+              }],
+            }, {
+              title: "touch",
+              subtitle: "Your Hands, Now in VR",
+              item_url: "https://www.oculus.com/en-us/touch/",               
+              image_url: SERVER_URL + "/assets/touch.png",
+              buttons: [{
+                type: "web_url",
+                url: "https://www.oculus.com/en-us/touch/",
+                title: "Open Web URL"
+              }, {
+                type: "postback",
+                title: "Call Postback",
+                payload: "Payload for second bubble",
+              }]
+            }]
+          }
+        }
+      }
+    };  
+
+    callSendAPI(messageData);
+  },
+
+  /*
+   * Send a receipt message using the Send API.
+   *
+   */
+  sendReceiptMessage: function (recipientId) {
+    // Generate a random receipt ID as the API requires a unique ID
+    var receiptId = "order" + Math.floor(Math.random()*1000);
+
+    var messageData = {
+      recipient: {
+        id: recipientId
+      },
+      message:{
+        attachment: {
+          type: "template",
+          payload: {
+            template_type: "receipt",
+            recipient_name: "Peter Chang",
+            order_number: receiptId,
+            currency: "USD",
+            payment_method: "Visa 1234",        
+            timestamp: "1428444852", 
+            elements: [{
+              title: "Oculus Rift",
+              subtitle: "Includes: headset, sensor, remote",
+              quantity: 1,
+              price: 599.00,
+              currency: "USD",
+              image_url: SERVER_URL + "/assets/riftsq.png"
+            }, {
+              title: "Samsung Gear VR",
+              subtitle: "Frost White",
+              quantity: 1,
+              price: 99.99,
+              currency: "USD",
+              image_url: SERVER_URL + "/assets/gearvrsq.png"
+            }],
+            address: {
+              street_1: "1 Hacker Way",
+              street_2: "",
+              city: "Menlo Park",
+              postal_code: "94025",
+              state: "CA",
+              country: "US"
+            },
+            summary: {
+              subtotal: 698.99,
+              shipping_cost: 20.00,
+              total_tax: 57.67,
+              total_cost: 626.66
+            },
+            adjustments: [{
+              name: "New Customer Discount",
+              amount: -50
+            }, {
+              name: "$100 Off Coupon",
+              amount: -100
+            }]
           }
         }
       }
@@ -157,22 +331,52 @@ module.exports = {
   },
 
   /*
-   * Send a video using the Send API.
+   * Send a message with Quick Reply buttons.
    *
    */
-  sendFileMessage: function (recipientId, url) {
+  sendQuickReply: function (recipientId) {
     var messageData = {
       recipient: {
         id: recipientId
       },
       message: {
-        attachment: {
-          type: "file",
-          payload: {
-            url: url
+        text: "What's your favorite movie genre?",
+        metadata: "DEVELOPER_DEFINED_METADATA",
+        quick_replies: [
+          {
+            "content_type":"text",
+            "title":"Action",
+            "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_ACTION"
+          },
+          {
+            "content_type":"text",
+            "title":"Comedy",
+            "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_COMEDY"
+          },
+          {
+            "content_type":"text",
+            "title":"Drama",
+            "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_DRAMA"
           }
-        }
+        ]
       }
+    };
+
+    callSendAPI(messageData);
+  },
+
+  /*
+   * Send a read receipt to indicate the message has been read
+   *
+   */
+  sendReadReceipt: function (recipientId) {
+    console.log("Sending a read receipt to mark message as seen");
+
+    var messageData = {
+      recipient: {
+        id: recipientId
+      },
+      sender_action: "mark_seen"
     };
 
     callSendAPI(messageData);
